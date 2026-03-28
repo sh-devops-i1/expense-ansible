53cc4f86233f104ad2665b41af2c31ea7ec092e3
shujath-git900 <shujath.devops@gmail.com>
2026-03-28 10:17:08 +0530

    uncomment playbook

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-env >/tmp/env
-#ansible-playbook -i $component-$env.mohdshuja.com, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+#env >/tmp/env
+ansible-playbook -i $component-$env.mohdshuja.com, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
