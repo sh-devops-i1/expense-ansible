71f11defaf861fe46148718e4b9051020119f4b8
shujath-git900 <shujath.devops@gmail.com>
2026-03-28 16:01:14 +0530

    Trying to retrieve secrets using ansible module

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-#ansible-playbook -i $component-$env.mohdshuja.com, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.mohdshuja.com, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
