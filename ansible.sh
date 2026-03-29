a8a05270a67fdd428ee9de7fb887654fd7d97fd3
shujath-git900 <shujath.devops@gmail.com>
2026-03-29 11:36:32 +0530

    @app.json file added

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.mohdshuja.com, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
+ansible-playbook -i $component-$env.mohdshuja.com, -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
